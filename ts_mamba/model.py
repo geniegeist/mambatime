9ba0920a462f441eadc0b93a62c0044bc6d1ec0d
Duc Nguyen <vduc@fastmail.com>
2025-11-17 10:36:07 +0100

    s

--- ts_mamba/model.py
+++ ts_mamba/model.py
@@ -89,7 +89,7 @@ class TimeseriesModel(nn.Module):
         super().__init__()
 
         # Encoder
-        self.encoder = nn.Linear(d_input, d_model, **factory_kwargs)
+        self.encoder = nn.Linear(d_input, d_model).to(device=device, dtype=dtype)
 
         self.layers = nn.ModuleList(
             [
@@ -114,7 +114,7 @@ class TimeseriesModel(nn.Module):
         )
 
         # Final output head for point prediction
-        self.decoder = nn.Linear(d_model, 1, **factory_kwargs)
+        self.decoder = nn.Linear(d_model, 1).to(device=device, dtype=dtype)
 
     def forward(self, x):
         """
